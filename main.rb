489bc7a962eb3948fd3c77361eefcc34bbeb2aa2
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-12-01 20:25:30 -0300

    listo el desafio

---
+++ main.rb
@@ -0,0 +1,5 @@
+require_relative 'appointment_classes'
+
+puts OneTimeAppointment.new('Desafío Latam', 'Trabajo', 14, 30, 4, 6, 2019)
+puts DailyAppointment.new('Desafío Latam', 'Educación', 8, 15)
+puts MonthlyAppointment.new('NASA', 'Aliens', 8, 15, 23)
